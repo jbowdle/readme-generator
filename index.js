40660581793e77e57d7c1aee813950996379cf30
jbowdle <johnwbowdle@gmail.com>
2023-04-04 12:10:38 -0500

    Created questions

--- index.js
+++ index.js
@@ -2,7 +2,54 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter your project title:",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a project description:",
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "Please enter installation instructions:",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter usage information:",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please enter contribution guidelines:",
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please enter test instructions:",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license would you like?",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter your GitHub username:",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address:",
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
