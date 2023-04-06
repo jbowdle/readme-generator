ddc4b15d6cbfe3ef68fda45acffa161300e814eb
jbowdle <johnwbowdle@gmail.com>
2023-04-06 18:16:13 -0500

    Added license link and section functions to use in markdownGenerator

--- index.js
+++ index.js
@@ -40,7 +40,17 @@ const questions = [
         type: "list",
         name: "license",
         message: "What license would you like?",
-        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+        choices: ["MIT", "APACHE 2.0", "BSD 3", "None"],
+    },
+    {
+        type: "input",
+        name: "year",
+        message: "Please enter the year for the copyright:",
+    },
+    {
+        type: "input",
+        name: "copyright",
+        message: "Please enter the name of the copyright holder:",
     },
     {
         type: "input",
